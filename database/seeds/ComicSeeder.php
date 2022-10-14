b77654b4e62848326613a8ad21dd0e09d88d7415
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-14 23:14:13 +0200

    add model, migration, seeder, ressource controller

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+use Faker\Generator as Faker;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
