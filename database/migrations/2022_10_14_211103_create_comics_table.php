20558708dead29a8612b707b85279da946b1343e
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-14 23:38:13 +0200

    completed seeder and migration

--- database/migrations/2022_10_14_211103_create_comics_table.php
+++ database/migrations/2022_10_14_211103_create_comics_table.php
@@ -15,9 +15,17 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
+            
 
     /**
      * Reverse the migrations.
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/', 'ComicController');
+Route::resource('comics', 'ComicController');
 
 
